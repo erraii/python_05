25c001bdfad9149268f8fdcf15acceebeade20db
erraii <eraycakiray@gmail.com>
2026-06-10 08:59:29 +0200

    Update data_processor.py

--- ex0/data_processor.py
+++ ex0/data_processor.py
@@ -18,7 +18,7 @@ class DataProcessor(abc.ABC):
 
     def output(self) -> tuple[int, str]:
         if not self._data_str:
-            raise IndexError("No data to output")
+            print("No data to output")
         return self._data_str.pop(0)
 
 
@@ -94,55 +94,55 @@ class LogProcessor(DataProcessor):
 def main() -> None:
     print("=== Code Nexus - Data Processor ===")
     print("\nTesting Numeric Processor...")
-    numProcess = NumericProcessor()
+    num_process = NumericProcessor()
     data_int = 42
-    success = numProcess.validate(data_int)
+    success = num_process.validate(data_int)
     print(f" Trying to validate input '{data_int}': {success}")
     data_str = "Hello"
-    success = numProcess.validate(data_str)
+    success = num_process.validate(data_str)
     print(f" Trying to validate input '{data_str}': {success}")
     data_str = "foo"
     print(" Test invalid ingestion of string ", end="")
     print(f"'{data_str}' without prior validation:")
     try:
-        numProcess.ingest(data_str)
+        num_process.ingest(data_str)
     except Exception as e:
         print(f" Got exception: {e}")
     int_list = [1, 2, 3, 4, 5]
-    if numProcess.validate(int_list):
+    if num_process.validate(int_list):
         print(f" Processing data: {int_list}")
-        numProcess.ingest(int_list)
+        num_process.ingest(int_list)
     print(" Extracting 3 values...")
     for i in range(0, 3):
-        output = numProcess.output()
+        output = num_process.output()
         print(f" Numeric value {output[0]}: {output[1]}")
     print("\nTesting Text Processor...")
-    textProcess = TextProcessor()
+    text_process = TextProcessor()
     data_int = 42
-    success = textProcess.validate(data_int)
+    success = text_process.validate(data_int)
     print(f" Trying to validate input '{data_int}': {success}")
     str_list = ["Hello", "Nexus", "World"]
-    if textProcess.validate(str_list):
+    if text_process.validate(str_list):
         print(f" Processing data: {str_list}")
-        textProcess.ingest(str_list)
+        text_process.ingest(str_list)
     print(" Extracting 1 value...")
     for i in range(0, 1):
-        output = textProcess.output()
+        output = text_process.output()
         print(f" Text value {output[0]}: {output[1]}")
-    logProcess = LogProcessor()
+    log_process = LogProcessor()
     data_str = "Hello"
-    success = logProcess.validate(data_str)
+    success = log_process.validate(data_str)
     print(f" Trying to validate input '{data_str}': {success}")
     dict_list = [{'log_level': 'NOTICE',
                   'log_message': 'Connection to server'},
                  {'log_level': 'ERROR',
                   'log_message': 'Unauthorized access!!'}]
-    if logProcess.validate(dict_list):
+    if log_process.validate(dict_list):
         print(f" Processing data: {dict_list}")
-        logProcess.ingest(dict_list)
+        log_process.ingest(dict_list)
     print(" Extracting 2 values...")
     for i in range(0, 2):
-        output = logProcess.output()
+        output = log_process.output()
         print(f" Log entry {output[0]}: {output[1]}")
 
 
